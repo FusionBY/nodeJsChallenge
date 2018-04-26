c20d30304717dea8dd4b467d0610884b5cfa26fd
Yahor Dauhaliuk <Yahor_Dauhaliuk@epam.com>
2018-04-26 16:21:43 +0300

    draft: finished task2

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+import EventEmitter from 'eventemitter2';
+const eventEmitter = new EventEmitter();
+
 import * as config from './config';
-import { Product, User } from './models';
+import { Product, User, DirWatcher, Importer } from './models';
 
 console.log('config-name:', config.name);
+
 const user = new User();
 const product = new Product();
+const dirWatcher = new DirWatcher('data', 3000, eventEmitter);
+const importer = new Importer(eventEmitter);
+
+eventEmitter.on('log:data', (data) => console.log(data));
