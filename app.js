18e39f1b20efe369671f645a1b887b63706b6074
Yahor Dauhaliuk <Yahor_Dauhaliuk@epam.com>
2018-06-07 11:55:26 +0300

    moved tasks 1-3 to tasks

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-import EventEmitter from 'events';
-const eventEmitter = new EventEmitter();
-
-import * as config from './config';
-import { Product, User } from './models';
-import { DirWatcher, Importer } from './modules';
-
-console.log('config-name:', config.name);
-
-new User();
-new Product();
-new DirWatcher('data', 300000, eventEmitter);
-new Importer(eventEmitter);
-
-eventEmitter.on('log:data', (data) => console.log(data));
-
-// Express Server
-
 import express from 'express';
 import cookieParser from './middlewares/cookieParser';
 import queryParser from './middlewares/queryParser';
